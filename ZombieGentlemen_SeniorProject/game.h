f51c185446d4d72f2ec355934ea5a11e2d4b8acc
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-08 08:40:30 +0000

    * added game.h, map.h, and object.h

---
+++ ZombieGentlemen_SeniorProject/game.h
@@ -0,0 +1,12 @@
+/**
+The game class will handle the bulk of the game logic
+This class will contian the game states, the map, the players, the characters
+etc.
+
+status: skeleton
+*/
+
+class game
+{
+	 
+};
---
+++ ZombieGentlemen_SeniorProject/map.h
@@ -0,0 +1,9 @@
+/**
+Map will be used to store the map image and the grid of the world
+
+status: skeleton
+*/
+
+class Map
+{
+};
