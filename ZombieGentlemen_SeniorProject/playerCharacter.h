f875d694a70c830a56b2f9edac0922710f17a7a5
shoutingchief@gmail.com <shoutingchief@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-15 22:58:36 +0000

    Am just up loading  my playerCharacter.h. Its not doing anything yet but I am working on it in a different project.

--- ZombieGentlemen_SeniorProject/playerCharacter.h
+++ ZombieGentlemen_SeniorProject/playerCharacter.h
@@ -1,1 +1,20 @@
 #pragma once
+class Player_Ch
+{
+private:
+
+public:
+	//getHealth()
+	void attack();
+	void defend();
+	void takeDamage();
+	//addItem()
+	//removeItem()
+	//addMoney()
+	//removeMoney()
+	//updateHelmet()
+	//updateArmor()
+	//calculateDamageResistance()
+	//calculateMovementSpeed()
+	//calculateMaxJumpHeight()
+};
