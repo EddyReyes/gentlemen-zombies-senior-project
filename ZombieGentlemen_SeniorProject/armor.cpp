67fa80732a727a3c9efacfc4e05c67fc99f1fcc2
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-16 23:51:29 +0000

    -Will Sup team, just added the basis for armor.h and armor.cpp files.

--- ZombieGentlemen_SeniorProject/armor.cpp
+++ ZombieGentlemen_SeniorProject/armor.cpp
@@ -1,1 +1,50 @@
 #include "armor.h"
+
+int PlayerArmor::damagedArmor(void)
+{
+	return 0;
+}
+int PlayerArmor::armorRegenerated(void)
+{
+	return 0;
+}
+int PlayerArmor::armorDepleted(void)
+{
+	return 0;
+}
+int PlayerArmor::armorPickup(void)
+{
+	return 0;
+}
+PlayerArmor::PlayerArmor()
+{
+	int playerArmor = 100;
+}
+PlayerArmor::~PlayerArmor()
+{
+
+}
+int NPCArmor::npcDamagedArmor(void)
+{
+	return 0;
+}
+int NPCArmor::npcArmorRegenerated(void)
+{
+	return 0;
+}
+int NPCArmor::npcArmorDepleted(void)
+{
+	return 0;
+}
+int NPCArmor::npcArmorPickup(void)
+{
+	return 0;
+}
+NPCArmor::NPCArmor()
+{
+	int npcArmor = 100;
+}
+NPCArmor::~NPCArmor()
+{
+
+}
--- ZombieGentlemen_SeniorProject/armor.h
+++ ZombieGentlemen_SeniorProject/armor.h
@@ -1,1 +1,36 @@
 #pragma once
+
+//work in progress, typical classes started here
+
+class PlayerArmor
+{
+	private:
+		int playerArmor;
+		int armorPoints;
+		int armorDamage;
+		int damageResistance;
+		int cost;
+	public:
+		int damagedArmor(void);
+		int armorRegenerated(void);
+		int armorDepleted(void);
+		int armorPickup(void);
+		PlayerArmor();
+		~PlayerArmor();
+};
+
+class NPCArmor
+{
+	private:
+		int npcArmor;
+		int npcArmorPoints;
+		int npcArmorDamage;
+		int npcDamageResistance;
+	public:
+		int npcDamagedArmor(void);
+		int npcArmorRegenerated(void);
+		int npcArmorDepleted(void);
+		int npcArmorPickup(void);
+		NPCArmor();
+		~NPCArmor();
+};
