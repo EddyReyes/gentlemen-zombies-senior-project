015599c323e1b4ebfa1446b3dec74e5bbf9a61ac
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-16 06:12:54 +0000

    Just updated the bare parts of the weapon.h and weapon.cpp files. I will add more on them later.

--- trunk/ZombieGentlemen_SeniorProject/weapon.h
+++ trunk/ZombieGentlemen_SeniorProject/weapon.h
@@ -2,10 +2,47 @@
 
 class Weapon
 {
-private:
-	int damage;
+	private:
+		int damage;
+		int hitPoints;
+		int weaponCost;
+		int weaponSpeed;
+	public:
+		int damageGiven(void);
+		int damageReceived(void);
+		int speed(void);
+		float cost(void);
+		int shovle();
+		Weapon();
+		~Weapon();	
+};
 
-public:
-	Weapon();
-	int shovle();
+class PlayerWeapon
+{
+	private:
+		int playerWeaponDamage;
+		int playerWeaponType;
+		int playerWeaponUpdate;
+	public:
+		int weaponType(void);
+		int weaponDamageUpdate(void);
+		int weaponPickup(void);
+		int weaponPickupUpdate(void);
+		PlayerWeapon();
+		~PlayerWeapon();
+};
+
+class NPCWeapon
+{
+	private:
+		int npcWeaponDamage;
+		int npcWeaponTypes;
+		int npcWeaponUpdate;
+	public:
+		int npcWeaponType(void);
+		int npcWeaponDamageUpdate(void);
+		int npcWeaponPickup(void);
+		int npcWeaponPickupUpdate(void);
+		NPCWeapon();
+		~NPCWeapon();
 };
