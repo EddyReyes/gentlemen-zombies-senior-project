74fab5d09ab3f14dd9f3f335b1ee610f0409b87a
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-16 21:12:26 +0000

    -Will
    I just updated the playerItem.h and .cpp files. I need to work on filling them in more.

--- trunk/ZombieGentlemen_SeniorProject/playerItem.h
+++ trunk/ZombieGentlemen_SeniorProject/playerItem.h
@@ -1,1 +1,21 @@
 #pragma once
+
+/* Implementing a single class for now that would take care of most of everything
+   for items.  Might update a new class. */
+
+class PlayerItem
+{
+	private:
+		int playerItem;
+		int itemType;
+		int itemUpdate;
+		int itemPickup;
+		int itemRandomSpawn;
+	public:
+		int itemToBeUsed();
+		int itemRespawn();
+		int generateRandomItemType();
+		int usedItem();
+		PlayerItem();
+		~PlayerItem();
+};
