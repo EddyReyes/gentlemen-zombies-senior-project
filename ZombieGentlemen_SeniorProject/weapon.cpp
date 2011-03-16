18e47d0cf70ed0bc7f95f7461cf25958227bdf98
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-03-16 06:12:54 +0000

    Just updated the bare parts of the weapon.h and weapon.cpp files. I will add more on them later.

--- ZombieGentlemen_SeniorProject/weapon.cpp
+++ ZombieGentlemen_SeniorProject/weapon.cpp
@@ -1,15 +1,78 @@
 #include "weapon.h"
 
-Weapon::Weapon()
+int Weapon::damageGiven(void)
 {
-	//damage points will always be set to zero. 
-	damage = 0;
-};
 
+	return 0;
+}
+int Weapon::damageReceived(void)
+{
+	return 0;
+}
+int Weapon::speed(void)
+{
+	return 0;
+}
+float Weapon::cost(void)
+{
+	return 0;
+}
 int Weapon::shovle()
 {
 	// the shovle only have 5 attack points. basicly it can only fight weak enemies.
 	int hitPoints = 5;
-	damage = hitPoints;
+	int damage = hitPoints;
 	return damage;
 };
+Weapon::Weapon()
+{
+	//damage points will always be set to zero. 
+	damage = 0;
+}
+Weapon::~Weapon()
+{
+}
+int PlayerWeapon::weaponType(void)
+{
+	return 0;
+}
+int PlayerWeapon::weaponDamageUpdate(void)
+{
+	return 0;
+}
+int PlayerWeapon::weaponPickup(void)
+{
+	return 0;
+}
+int PlayerWeapon::weaponPickupUpdate(void)
+{
+	return 0;
+}
+PlayerWeapon::PlayerWeapon()
+{
+}
+PlayerWeapon::~PlayerWeapon()
+{
+}
+int NPCWeapon::npcWeaponType(void)
+{
+	return 0;
+}
+int NPCWeapon::npcWeaponDamageUpdate(void)
+{
+	return 0;
+}
+int NPCWeapon::npcWeaponPickup(void)
+{
+	return 0;
+}
+int NPCWeapon::npcWeaponPickupUpdate(void)
+{
+	return 0;
+}
+NPCWeapon::NPCWeapon()
+{
+}
+NPCWeapon::~NPCWeapon()
+{
+}
