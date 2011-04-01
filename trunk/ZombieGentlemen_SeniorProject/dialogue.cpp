8593eaea003c162a7054451f1f67b620aaddd304
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-01 00:31:25 +0000

    * created prototype of Dialogue class

--- trunk/ZombieGentlemen_SeniorProject/dialogue.cpp
+++ trunk/ZombieGentlemen_SeniorProject/dialogue.cpp
@@ -1,1 +1,49 @@
 #include "dialogue.h"
+
+
+
+// when you need pd3ddevice use *dxMgr->getDevice()
+
+Dialogue::Dialogue(dxManager * a_dxMgr, std::string filename)
+{
+	dxMgr = a_dxMgr;
+	dialogText = new std::string;
+	fontName = new std::string;
+	box = new HudImage(dxMgr, filename);
+	fontColor = new D3DCOLOR;
+	textBox = new RECT;
+
+	// by default set font to arial
+	fontSize = 20;
+	italic = false;
+	setFontName("Arial");
+	
+}
+void Dialogue::setFont()
+{
+	//Create a D3DX font object
+	D3DXCreateFont( *dxMgr->getDevice(),
+					fontSize,			// SIZE
+					0,
+					FW_BOLD,	// WEIGHT
+					0,
+					italic,		// Italic
+					DEFAULT_CHARSET,
+					OUT_DEFAULT_PRECIS,
+					DEFAULT_QUALITY,
+					DEFAULT_PITCH | FF_DONTCARE,
+					TEXT(fontName->c_str()),
+					&font );
+}
+void Dialogue::setFontName(std::string a_fontName)
+{
+	*fontName = a_fontName;
+	setFont();
+}
+void Dialogue::setFontSize(int a_size)
+{
+	fontSize = a_size;
+	setFont();
+}
+// set font color using D3DCOLOR_ARGB(a, r, g, b)
+//*fontColor = D3DCOLOR_ARGB(a, r, g, b) 
