b8e988b372dac34aa2234a9aeed3f2a33dac5a1f
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-01 00:31:25 +0000

    * created prototype of Dialogue class

--- ZombieGentlemen_SeniorProject/dialogue.h
+++ ZombieGentlemen_SeniorProject/dialogue.h
@@ -1,11 +1,30 @@
 //Dialogue class
+#include <string.h>
+#include "HudImage.h"
+#include "dxManager.h"
 
 class Dialogue
 {
 	private:
-		char text;
+		std::string * dialogText;
+		std::string * fontName;
+		HudImage * box;
+		ID3DXFont * font;
+		dxManager * dxMgr;
+		D3DCOLOR * fontColor;
+		RECT * textBox;
+		int fontSize;
+		bool italic;
+
 	public:
-		Dialogue();
+		Dialogue(dxManager * a_dxMgr, std::string filename);
 		~Dialogue();
-		void dialogText();
+		void setFont();
+		void setFontName(std::string a_fontName);
+		void setFontSize(int a_size);
+		void setFontColor();
+		void setImage();
+		void setDialogue();
+		void setRect(int top, int bottom, int left, int right);
+		void toggleItalic();
 };
