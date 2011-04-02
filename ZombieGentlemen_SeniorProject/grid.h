668bccd21415e309dff816b2a28a9533ab1ed4d1
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-02 22:12:30 +0000

    * added solution file to ignore list

--- ZombieGentlemen_SeniorProject/grid.h
+++ ZombieGentlemen_SeniorProject/grid.h
@@ -1,53 +1,6 @@
 #include "dxManager.h"
 #include "object.h"
 
-#define debug
-#ifndef debug
-class grid
-{
-private:
-
-	#ifndef debug
-
-	LPD3DXLINE * XLines;
-	LPD3DXLINE * YLines;
-	D3DXVECTOR2 ** XlinesVertexList;
-	D3DXVECTOR2 ** YlinesVertexList;
-	float XAxisLimit, YAxisLimit;
-
-	
-	float gridScale;
-	int YLength, XLength;
-	
-	
-	
-	//Axis Parameters
-	float XAxis, YAxis;
-
-	
-	#endif
-	
-	bool gridOn;
-	
-	// nodes
-	D3DXVECTOR2 ** nodes;
-	D3DXVECTOR2 * center;
-
-	void releaseLines();
-	void releaseVertexLists();
-	dxManager * dxMgr;
-public:
-	grid();
-	grid(float a_gridScale , float a_XAxisLimit, float a_YAxisLimit, dxManager * a_dxMgr);
-	~grid();
-	void initGrid();
-	void toggleGrid();
-	void drawGrid();
-	bool isGridOn();
-	void changeGridScale(float a_gridScale);
-};
-#endif
-
 class grid
 {
 private:
