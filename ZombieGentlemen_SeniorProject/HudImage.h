10c8484901dc89c38f613d882941c991ed33c594
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-06 04:01:58 +0000

    * made changes to HudImage      Removed redundant constructors

--- ZombieGentlemen_SeniorProject/HudImage.h
+++ ZombieGentlemen_SeniorProject/HudImage.h
@@ -21,11 +21,7 @@ private:
 public:
 	HudImage();
 	HudImage(dxManager * a_dxMgr, std::string filename);
-	HudImage(dxManager * a_dxMgr, std::string filename, int a_spriteRows);
-	HudImage(dxManager * a_dxMgr, std::string filename, int a_spriteRows, int a_spriteColumns);
 	HudImage(dxManager * a_dxMgr, LPDIRECT3DTEXTURE9 * a_image, D3DXIMAGE_INFO * a_imageInfo);
-	HudImage(dxManager * a_dxMgr, LPDIRECT3DTEXTURE9 * a_image, D3DXIMAGE_INFO * a_imageInfo, int a_spriteRows);
-	HudImage(dxManager * a_dxMgr, LPDIRECT3DTEXTURE9 * a_image, D3DXIMAGE_INFO * a_imageInfo, int a_spriteRows, int a_spriteColumns);
 	~HudImage();
 	void releaseImage();
 	void initializeSprite(std::string filename);
@@ -40,6 +36,7 @@ public:
 	void setSpriteColumns(int a_spriteColumns);
 	void setSpriteRows(int a_spriteRows);
 	void guessSpriteColumns();
+	void setRowsColumns(int a_spriteRows, int a_spriteColumns);
 	void selectSpriteSource(int spriteIndex);
 	void selectSpriteSource(int a_spriteRow, int a_spriteColumn);
 	LPDIRECT3DTEXTURE9 * getTexture();
