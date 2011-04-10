b3449bed7a0464e23aefbc4d74bb598fe64af7cc
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-10 00:47:07 +0000

    * updated cube rendering

--- trunk/ZombieGentlemen_SeniorProject/dxCube.cpp
+++ trunk/ZombieGentlemen_SeniorProject/dxCube.cpp
@@ -436,7 +436,7 @@ void dxCube::draw()
 			pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 16, 2 );
 			pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 20, 2 );
 
-			pd3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_CCW );
+			pd3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_CW );
 			pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP,  4, 2 );
 			pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 12, 2 );
 			pd3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_CCW );
