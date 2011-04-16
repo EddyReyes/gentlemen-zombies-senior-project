bfe51fc5ae4246928886adb2e82db7c1d6bea74c
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-16 20:24:43 +0000

    * added collisionMap

---
+++ ZombieGentlemen_SeniorProject/collisionMap.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "grid.h"
+#include <stdio.h>
+#include <fstream>
+#include <ctype.h>
+#include <string.h>
+
+class collisionMap
+{
+private:
+	char ** map;
+	collisionRect * rects;
+	grid * m_grid;
+	float * scale;
+public: 
+	collisionMap()
+	{
+		map = NULL;
+		rects = NULL;
+		grid = NULL;
+		scale = NULL;
+	}
+	~collisionMap(){}
+
+	initCollMap(char * filename, cubeMap * sourceMap)
+	{
+		//m_grid = sourceMap->getGrid();
+		//scale = sourceMap->getScale();
+
+		std::fstream file(filename);
+		// the size of the data we are going to create
+		file >> height >> width
+		/*printf("file size %dx%d\n", m_width, m_height);*/
+		map = new char * [height];
+		for(int c = 0; c < height; c++){
+			map[c] = new char [width];}
+		// get the data from the file into the 2D array
+		int c;
+		for(int y = 0; y < height; ++y){
+			for(int x = 0; x < width; ++x){
+				//putchar(fileData[y][x]);
+				c = file.get();
+				if(c == '\n' || c == '\r'){
+					x--;}
+				else{
+					map[y][x] = c;
+				}
+			}
+		}
+	}
+}
