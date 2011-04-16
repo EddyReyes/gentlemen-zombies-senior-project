b8282620727f7e7052aa3b25fe18d70f7fdb5c68
thossnider <thossnider@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-16 21:32:52 +0000

    * Added to the interior.h class.

--- ZombieGentlemen_SeniorProject/interior.h
+++ ZombieGentlemen_SeniorProject/interior.h
@@ -1,2 +1,25 @@
 #pragma once
 #include "DXText.h"
+#include "HudImage.h"
+#include "playerCharacter.h"
+
+
+class interior
+{
+
+private:
+DXText * playerMoney;
+dxManager * dxMgr;
+
+
+
+public:
+interior();
+interior(dxManager * a_dxMgr);
+~interior();
+void interiorDraw();
+void interiorBackground();
+void interiorForeground();
+void dialogueBox();
+void updateMenu();
+};
