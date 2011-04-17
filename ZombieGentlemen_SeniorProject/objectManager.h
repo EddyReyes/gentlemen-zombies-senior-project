f70457b5ca6139fadced8eabfd207fb3eb57e621
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-17 04:52:47 +0000

    * modified collisionMap and objectManager

--- ZombieGentlemen_SeniorProject/objectManager.h
+++ ZombieGentlemen_SeniorProject/objectManager.h
@@ -2,55 +2,53 @@
 #include "object.h"
 #include "objectVector.h"
 #include "collisionMap.h"
-//#define test
 
 class objectManager
 {
-public:
-	
-	void initColMap(char * filename, cubeMap * map)
-	{
-		colMap.initCollMap(filename, map);
-		colMap.generateRects();
-	};
-
-
-#ifndef test
-	objectManager()
-	{
-		list.initList(1);
-		colRects = new collisionRect[10];
-	}
-	void insert(object *a_obj) //takes in object to put into the list
-	{
-		list.add(a_obj);//inserts the object to the back of the list
-	}
-	object* get(int index)
-	{
-		return list.get(index); //gets an object at an index
-	}
-	void remove(int index)//removes object from list
-	{
-		list.remove(index);
-	}
-	int getsize() {return list.getSize();}//returns the size of the list
-	void destroyobject(int index)
-	{
-		list.destroyObject(index);
-	}
-	~objectManager()
-	{
-		list.~objectList();
-		delete colRects;
-	}
-	int endoflist()
-	{
-		return list.endOfList();
-	}
 private:
 	objectList list; //list that will hold all the objects
 	collisionMap colMap;
-	collisionRect * colRects;
+	collisionRect ** colMapRects;
+
+public:
 	
-#endif
+	objectManager()
+	{
+		/*list.initList(1);
+		colRects = new collisionRect[10];*/
+	}
+
+	void initColMap(char * filename, cubeMap * map)
+	{
+		colMap.initCollMap(filename, map);
+		colMapRects = colMap.getList();
+	};
+
+	//void insert(object *a_obj) //takes in object to put into the list
+	//{
+	//	list.add(a_obj);//inserts the object to the back of the list
+	//}
+	//object* get(int index)
+	//{
+	//	return list.get(index); //gets an object at an index
+	//}
+	//void remove(int index)//removes object from list
+	//{
+	//	list.remove(index);
+	//}
+	//int getsize() {return list.getSize();}//returns the size of the list
+	//void destroyobject(int index)
+	//{
+	//	list.destroyObject(index);
+	//}
+	~objectManager()
+	{
+		/*list.~objectList();
+		delete colRects;*/
+	}
+	/*int endoflist()
+	{
+		return list.endOfList();
+	}*/
+
 };
