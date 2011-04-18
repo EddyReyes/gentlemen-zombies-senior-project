3b81198235f95a1c2215c50b55265a75121201bf
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-18 04:45:25 +0000

    * fixed imageManager

--- trunk/ZombieGentlemen_SeniorProject/imageManager.h
+++ trunk/ZombieGentlemen_SeniorProject/imageManager.h
@@ -12,25 +12,23 @@ class imageManager
 private:
 	LPDIRECT3DTEXTURE9 * images;	// image texture
 	D3DXIMAGE_INFO ** imageInfo;	// contains image parameters
-	char ** textFile;
+	std::string ** fileNames;
 	dxManager * dxMgr;
-	int fileHeight, fileWidth, arraySize;
+	int size;
 
 public:
 	imageManager()
 	{
 		images = NULL;
 		imageInfo = NULL;
-		textFile = NULL;
+		fileNames = NULL;
 		dxMgr = NULL;
-		fileHeight = 0;
-		fileWidth = 0;
-		arraySize = 0;
+		size = 0;
 	}
 	~imageManager()
 	{
 		// destroy all images
-		for(int y  = 0; y < fileHeight; y ++)
+		for(int y  = 0; y < size; y ++)
 		{
 			if(images[y] != NULL)
 			{
@@ -41,7 +39,7 @@ public:
 		}
 		delete [] images;
 		// destroy all imageInfo
-		for(int y  = 0; y < fileHeight; y ++)
+		for(int y  = 0; y < size; y ++)
 		{
 			if(imageInfo[y] != NULL)
 			{
@@ -51,15 +49,15 @@ public:
 			}
 		}
 		delete [] imageInfo;
-		// destroy 2D array of characters
-		if(textFile)
+		// destroy array of strings
+		if(fileNames)
 		{
-			for(int i = 0; i < fileHeight; i++)
+			for(int i = 0; i < size; i++)
 			{
-				delete [] textFile[i];
+				fileNames[i]->~basic_string();
 			}
-			delete [] textFile;
-			textFile = NULL;
+			delete [] fileNames;
+			fileNames = NULL;
 		}
 	}
 	void initImageManager(char * filename, dxManager * a_dxMgr)
@@ -73,75 +71,51 @@ public:
 	{
 		fstream file(filename);
 		// capture the height(number of rows) and width(number of chars)
-		file >> fileHeight >> fileWidth;
+		file >> size;
 		// use height to create the respective rows required
-		textFile = new char * [fileHeight];
-		// loop through array and create collumns and assign NULL to all chars
-		for(int y = 0; y < fileHeight; y++){
-			textFile[y] = new char [fileWidth];
-			for(int x = 0; x < fileWidth; x++)
-			{textFile[y][x] = NULL;}
+		fileNames = new std::string * [size];
+		for(int y = 0; y < size; y++)
+		{
+			fileNames[y] = new std::string;
 		}
+		file.ignore(1,0);
 		// get the data from the file into the 2D array
-		int c;
-		bool newLine = false;
-		for(int y = 0; y < fileHeight; ++y){
-			newLine = false;
-			for(int x = 0; x < fileWidth && !newLine; ++x){
-				c = file.get();
-				newLine = false;
-				if(c == '\n' || c == '\r'){
-					x--;
-					newLine = true;
-				}
-				else{
-					textFile[y][x] = c;
-				}
-			}
+		for(int y = 0; y < size; ++y){
+			char fileName[100];
+			file.getline(fileName, 100);
+			fileNames[y]->append(fileName);
+			ZeroMemory(fileName, sizeof(fileName));
 		}
 	}
 
 	void initImagesArray()
 	{
-		// find size necessary to fill in array
-		for(int y = 0; y < fileHeight && textFile[y][0] !=  NULL; y++)
-		{arraySize++;}
 		// create an array of the right size for image pointers
-		images = new LPDIRECT3DTEXTURE9 [arraySize];
-		for(int i = 0; i < arraySize; i++)
+		images = new LPDIRECT3DTEXTURE9 [size];
+		for(int i = 0; i < size; i++)
 		{images[i] = NULL;} // fill all image pointers with NULL
 		// create an array of the right size for imageInfo pointers
-		imageInfo = new D3DXIMAGE_INFO * [arraySize];
-		for(int i = 0; i < arraySize; i++)
+		imageInfo = new D3DXIMAGE_INFO * [size];
+		for(int i = 0; i < size; i++)
 		{imageInfo[i] = NULL;} // zero out the imageInfo struct
 	}
 
 	void loadImages()
 	{
-		// create a string to hold the file name characters
-		std::string str;
-		// loop through 2D char array
-		for(int y = 0; y < fileHeight && textFile[y][0] !=  NULL; y++)
+		// loop through size
+		for(int y = 0; y < size; y++)
 		{
-			// loop through array row to get each character
-			// will exit loop if the next character is NULL
-			for(int x = 0; x < fileWidth && textFile[y][x] !=  NULL; x++)
-			{
-				str.push_back(textFile[y][x]); // add characters from 2D char array to string
-			}
-			// after the string has collected characters from row the file name has
-			// been captured in the string, and it can be used to load an image
-			// file into memory, and the file info can be captured
-			D3DXCreateTextureFromFile(pd3dDevice, str.c_str(), &(images[y]));
-			D3DXGetImageInfoFromFile(str.c_str(), imageInfo[y]);
-			// the string is cleared for use with the next row of characters
-			str.clear();
+			// load texture from file in file name array
+			D3DXCreateTextureFromFile(pd3dDevice, fileNames[y]->c_str(),&(images[y]));
+			// laod image info from file in file name array
+			D3DXGetImageInfoFromFile(fileNames[y]->c_str(), imageInfo[y]);
 		}
 	}
+
 	LPDIRECT3DTEXTURE9 * getImage(int index)
 	{
 		// check if the index is out of bounds
-		if(index < 0 || index > arraySize)
+		if(index < 0 || index > size)
 		{return NULL;} // if out of bounds return NULL
 		// if index is within memory bounds return a pointer
 		else{return &(images[index]);}
@@ -149,7 +123,7 @@ public:
 	D3DXIMAGE_INFO * getImageInfo(int index)
 	{
 		// check if the index is out of bounds
-		if(index < 0 || index > arraySize)
+		if(index < 0 || index > size)
 		{return NULL;} // if out of bounds return NULL
 		// if index is within memory bounds return a pointer
 		else{return imageInfo[index];}
