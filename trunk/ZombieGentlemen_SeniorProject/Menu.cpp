c12fde37fdbf085b0bd4aa7c134cf13caf2c9d2e
mknoche@gmail.com <mknoche@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-26 23:33:56 +0000

    For the most part its a finished menu am going to work on the store and get it to work with it, also took out some state that I thought i needed but really don't =P

--- trunk/ZombieGentlemen_SeniorProject/Menu.cpp
+++ trunk/ZombieGentlemen_SeniorProject/Menu.cpp
@@ -1,10 +1,7 @@
 #include "Menu.h"
-
-/*											*\
-*											*
-*This will house the Menu State code.	*
-\*											*/
-
+/*
+TODO: make a few more constructors, make it less magic numbery, figure out how to make a Menu within a menu
+*/
 
 Menu::Menu()
 {
@@ -14,29 +11,34 @@ Menu::Menu()
 	option_txt = NULL;
 	options = NULL;
 }
-Menu::Menu(dxManager * a_dxMgr)
+Menu::Menu(dxManager * a_dxMgr,char* menuArt,char* menuOptions)//takes the the directx manager and initialized everything
 {
+	//creates a new image manager from a txt file and creates a background image out of the first element
 	selected = 0;
 	imgMan = new imageManager();
-	imgMan->initImageManager("menuItems.txt",a_dxMgr);
+	imgMan->initImageManager(menuArt,a_dxMgr);
 	background = new HudImage(a_dxMgr,imgMan->getImage(0),imgMan->getImageInfo(0));
 
+	//is a string array that reads a file of all the dialog and creates options
 	option_txt = new stringArray;
-	option_txt->loadFromTextFile("Scatta.txt");
+	option_txt->loadFromTextFile(menuOptions);
 
+	//creates text boxes out of the information given from image manager
 	options = new DXText* [option_txt->getSize()];
-	for(int c=0;c<num_of_options;++c)
+	for(int c=0;c<option_txt->getSize();++c)
 	{
 		options[c] = new DXText(a_dxMgr,imgMan->getImage(1),imgMan->getImageInfo(1));
-		//options[c]->setDialog(option_txt->getStringAt(c));
+		options[c]->setDialog(option_txt->getStringAt(c));
 	}
-	setParam(300,300,150,150);
+	//default parameters
+	const float height = 300, width = 300;
+	setParam(height,width,640/2,480/2);
 }
 
 void Menu::Draw()
 {
 	background->draw();
-	for(int a=0;a<num_of_options;++a)
+	for(int a=0;a<option_txt->getSize();++a)
 		options[a]->draw();
 }
 
@@ -47,12 +49,11 @@ void Menu::dialogueBox()
 void Menu::setParam(float w, float h, float a_x, float a_y)
 {
 	background->setParameters(w,h,a_x,a_y);
-	for(int q=0;q<num_of_options;++q)
+	for(int q=0;q<option_txt->getSize();++q)
 	{
-		options[q]->setTextBoxParameters(w,h,a_x,a_y+(q*100),0);
-		options[q]->textInfo("Arial", 18,
-					 D3DCOLOR_ARGB(255, 0, 0, 0),
-					 option_txt->getStringAt(q));
+		//offset seemingly random
+		options[q]->setTextBoxParameters(w/2,h/4,a_x,a_y+(q*100),10);
+		options[q]->setFontColor(0xff,0x00,0x00,0x00);//sets the color to black in hex
 	}
 }
 void Menu::questDialogue(std::string filename)
@@ -63,31 +64,40 @@ void Menu::storeMenu(std::string filename)
 {
 
 }
-void Menu::update(BYTE* keystate,int now,int * keylag)
+int Menu::update(BYTE* keystate,int now,int * keylag)
 {
+	
+	//key allows you to scroll through the options
 	if(keystate[DIK_T] & 0x80)
 	{
 		if(now - keylag[DIK_T] > 150)
 		{
 			if (selected == 0) 
 			{
-				options[0]->getimg()->shareImage(imgMan->getImage(2),imgMan->getImageInfo(2));
-				selected = 1;
+				selected++;
+				options[selected-1]->getimg()->shareImage(imgMan->getImage(2),imgMan->getImageInfo(2));
 			}
-			else if(selected == 1)
+			else if(selected < option_txt->getSize())
 			{
-				options[0]->getimg()->shareImage(imgMan->getImage(1),imgMan->getImageInfo(1));
-				options[1]->getimg()->shareImage(imgMan->getImage(2),imgMan->getImageInfo(2));
-				selected = 2;
+				options[selected-1]->getimg()->shareImage(imgMan->getImage(1),imgMan->getImageInfo(1));
+				options[selected]->getimg()->shareImage(imgMan->getImage(2),imgMan->getImageInfo(2));
+				selected++;
 			}
 			else
 			{
-				options[1]->getimg()->shareImage(imgMan->getImage(1),imgMan->getImageInfo(1));
-				selected =0;
+				options[selected-1]->getimg()->shareImage(imgMan->getImage(1),imgMan->getImageInfo(1));
+				selected = 0;
 			}
 			keylag[DIK_T] = now;
 		}
+		
 	}
+	//key that says that an option was hit
+	if(keystate[DIK_E] & 0x80)
+	{
+		return selected;
+	}
+	return 0;
 }
 Menu::~Menu()
 {
