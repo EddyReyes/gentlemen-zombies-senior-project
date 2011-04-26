7af4be8599c2c5bff4345f8a358c2182b1b50e66
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-26 01:40:13 +0000

    * dxText parameter loading is now scripted

--- ZombieGentlemen_SeniorProject/game.cpp
+++ ZombieGentlemen_SeniorProject/game.cpp
@@ -580,11 +580,6 @@ bool game::initGame(dxManager * a_dxMgr, directInput * a_inputMgr, sound * a_sou
 
 	// initialize FPS display data
 	FPSText = new DXText(dxMgr, "images/BlackTextBox.bmp");
-	//FPSText = new DXText(dxMgr);
-	/*FPSText->textInfo("Arial", 18,
-					 D3DCOLOR_ARGB(255, 255, 255, 255),
-					 "Loading...");
-	FPSText->setTextBoxParameters(200, 50, 20, 500, 8);*/
 	FPSText->loadFromTxtFile("textParameters.txt");
 	FPSText->setDialog("Loading...");
 
