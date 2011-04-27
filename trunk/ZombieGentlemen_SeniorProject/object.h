b484cd5b5534368c2f6953aced7cddf0268c69e0
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-27 00:12:38 +0000

    * updated game.h to include all the new files

--- trunk/ZombieGentlemen_SeniorProject/object.h
+++ trunk/ZombieGentlemen_SeniorProject/object.h
@@ -8,8 +8,6 @@
 #include <ctype.h>
 #include <string.h>
 
-
-#define debug
 class object
 {
 protected:
@@ -313,109 +311,4 @@ public:
 		}
 		return NULL;
 	}
-
-
-#ifndef debug
-protected:
-	XYPlane *  image;
-	collisionRect * collRect;
-	collisionRect * targetCollRect;
-	bool collisionToggle;
-	dxManager* dxMgr;
-	float movement;
-
-public:
-	object(dxManager* a_dxMgr, std::string imgFile, char * textFile)
-	{
-		dxMgr = a_dxMgr;
-		image = new XYPlane(dxMgr, imgFile);
-		collRect = new collisionRect();
-		collRect->initRect(image);
-		loadParameters(textFile);
-		movement = 0.005f;
-		collisionToggle = true;
-	}
-	~object()
-	{
-		image->~XYPlane();
-		image = NULL;
-		collRect->~collisionRect();
-		collRect = NULL;
-	}
-	/********************************************************************************
-	* loadParameters
-	* loads the parameters for any object using a text file
-	* instructions: send in a text file with the parameters in this order, separated by 
-	* a space
-	* x, y, z, width, height, rows, columns, left offset, top offset, right offset, 
-	* bottom offset
-	********************************************************************************/
-	void loadParameters(char * filename)
-	{
-		std::fstream file(filename);
-		D3DXVECTOR3 pos;
-		float width, height;
-		int imageRows, imageColumns;
-		FloatRect offset;
-		// the size of the data we are going to create
-		file >> pos.x >> pos.y >> pos.z >> width >> height >> imageRows >> imageColumns
-			>> offset.left >> offset.top >> offset.right >> offset.bottom;
-		image->setParam(pos.x, pos.y, pos.z, width, height);
-		image->setImageRowsColumns(imageRows, imageColumns);
-		collRect->setOffset(&offset);
-	}
-	void loadParameters(float x, float y, float z, float width, float height,
-		 int imageRows, int imageColumns, float leftOffset, float topOffset, float rightOffset,
-		 float bottomOffset)
-	{
-		image->setParam(x, y, z, width, height);
-		image->setImageRowsColumns(imageRows, imageColumns);
-		collRect->setOffset(leftOffset, rightOffset, topOffset, bottomOffset);
-	}
-	void draw()
-	{
-		image->draw();
-	}
-	/************************************************************************************
-	* handleCollision
-	* returns true if movement is valid, flase if movement is invalid and moves object back
-	************************************************************************************/
-	bool handleCollision(float a_x, float a_y, float a_z)
-	{
-		if(collisionToggle)
-		{
-			D3DXVECTOR3 pos = *image->getPosition();
-			image->setPosition(a_x, a_y, a_z);
-			collRect->update();
-			if(collRect->collided(targetCollRect->getRect()))
-			{
-				image->setPosition(pos);
-				collRect->update();
-				return false;
-			}
-			return true;
-		}
-		else
-		{
-			image->setPosition(a_x, a_y, a_z);
-			collRect->update();
-			return true;
-		}
-	}
-	collisionRect * getCollisionRect(){return collRect;}
-	XYPlane * getXYPlane(){return image;}
-	void setPosition(float a_x, float a_y, float a_z)
-	{
-		image->setPosition(a_x, a_y, a_z);
-		collRect->update();
-	}
-	void setTargetCollision(collisionRect * a_collRect)
-	{
-		targetCollRect = a_collRect;
-	}
-	void toggleImage(){image->toggleImage();}
-	void toggleCollision(){collisionToggle = !collisionToggle;}
-	void collisionToggleOn(){collisionToggle = true;}
-	void collisionToggleOff(){collisionToggle = false;}
-#endif
 };
