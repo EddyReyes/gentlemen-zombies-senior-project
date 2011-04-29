9e68eab72ec1b8ec466b3afc138b80d4393de83a
mknoche@gmail.com <mknoche@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-04-29 20:35:55 +0000

    committing level stuff for eddy to help me with also fixed the problems that might be associated with it

---
+++ ZombieGentlemen_SeniorProject/level.cpp
@@ -0,0 +1,44 @@
+#include "level.h"
+
+
+level::level()
+{
+	ObjMan = NULL;
+	files = NULL;
+	m_map = NULL;
+}
+level::level(dxManager* a_dxMgr,char* initfiles)//will load a file with the names of files in it
+{
+	//puts the files into a string array
+	files = new stringArray();
+	files->loadFromTextFile(initfiles);
+
+	//initialize the data with it
+	//code that isn't working correctly commented so project won't crash
+	//m_map = new cubeMap(files->getStringAt(0).c_str(),files->getStringAt(1),a_dxMgr);
+
+	ObjMan = new objectManager();
+	//ObjMan->initColMap(
+}
+void level::loadlevel(char* initfiles)
+{
+}
+void level::setMusic(char* sound)
+{
+
+}
+void level::draw()
+{
+}
+void level::handlecollision()
+{
+
+}
+void level::moveObject()
+{
+}
+level::~level()
+{
+	//ObjMan->~objectManager();
+	files->~stringArray();
+}
