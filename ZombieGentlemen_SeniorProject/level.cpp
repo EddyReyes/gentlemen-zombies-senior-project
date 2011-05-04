e4de770c9154c7e4c3ed07145f640ea19666e505
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-04 07:21:02 +0000

    level now handles its own input

--- ZombieGentlemen_SeniorProject/level.cpp
+++ ZombieGentlemen_SeniorProject/level.cpp
@@ -3,7 +3,7 @@
 
 level::level()
 {
-	ObjMan = NULL;
+	objMgr = NULL;
 	files = NULL;
 	m_map = NULL;
 }
@@ -20,11 +20,11 @@ void level::initLevel(dxManager* a_dxMgr, std::string initFiles)
 	//initialize the data with it
 	m_map = new cubeMap(files->getStringAt(0),files->getStringAt(1),a_dxMgr);
 
-	ObjMan = new objectManager();
-	ObjMan->initObjectMgr(a_dxMgr);
-	ObjMan->initColMap(files->getStringAt(2), m_map);
-	ObjMan->initImages(files->getStringAt(3));
-	ObjMan->loadObjectsFromTxtFile(files->getStringAt(4));
+	objMgr = new objectManager();
+	objMgr->initObjectMgr(a_dxMgr);
+	objMgr->initColMap(files->getStringAt(2), m_map);
+	objMgr->initImages(files->getStringAt(3));
+	objMgr->loadObjectsFromTxtFile(files->getStringAt(4));
 	backGrnd = new background();
 	backGrnd->initBackground(a_dxMgr, files->getStringAt(5));
 }
@@ -36,8 +36,8 @@ void level::initLevel(dxManager* a_dxMgr, std::string initFiles)
 ******************************************************************/
 void level::update(float updateTime)
 {
-	ObjMan->updatePhysics(updateTime);
-	ObjMan->handleCollision();
+	objMgr->updatePhysics(updateTime);
+	objMgr->handleCollision();
 }
 void level::setMusic(char* sound)
 {
@@ -50,15 +50,187 @@ void level::draw()
 {
 	m_map->draw();
 	backGrnd->draw();
-	ObjMan->draw();
+	objMgr->draw();
 }
+void level::handleInput(inputData * input, int now)
+{
+	// down key
+	if ((input->keystate[DIK_UP] & 0x80) || (input->keystate[DIK_W] & 0x80))
+	{
+		if(objMgr->getObject()->getPhysics())
+		{
+			objMgr->getObject()->getPhysics()->setYVelocity(12.0f);
+		}
+		else
+			objMgr->moveObject(D3DXVECTOR3(0.0f, 0.05f, 0.0f));
+	}
+
+	// down key
+	if ((input->keystate[DIK_DOWN] & 0x80)|| (input->keystate[DIK_S] & 0x80))
+	{
+		if(!objMgr->getObject()->getPhysics())
+			objMgr->moveObject(D3DXVECTOR3(0.0f, -0.05f, 0.0f));
+	}
+	
+	// left key
+	if ((input->keystate[DIK_LEFT] & 0x80) || (input->keystate[DIK_A] & 0x80))
+	{
+		if(objMgr->getObject()->getPhysics())
+		{
+			if(objMgr->getObject()->getPhysics()->canMoveLeft())
+			{
+				objMgr->getObject()->getPhysics()->walkingOn();
+				objMgr->getObject()->getPhysics()->setXVelocity(-6.0f);
+			}
+		}
+		else
+			objMgr->moveObject(D3DXVECTOR3(-0.05f, 0.0f, 0.0f));
+	}
+
+	// right key
+	if ((input->keystate[DIK_RIGHT] & 0x80) || (input->keystate[DIK_D] & 0x80))
+	{
+		if(objMgr->getObject()->getPhysics())
+		{
+			if(objMgr->getObject()->getPhysics()->canMoveRight())
+			{
+				objMgr->getObject()->getPhysics()->walkingOn();
+				objMgr->getObject()->getPhysics()->setXVelocity(6.0f);
+			}
+		}
+		else
+			objMgr->moveObject(D3DXVECTOR3(0.05f, 0.0f, 0.0f));
+	}
+
+	// if neither left or right key are active
+	if (!((input->keystate[DIK_LEFT] & 0x80) || (input->keystate[DIK_A] & 0x80))
+		&& !((input->keystate[DIK_RIGHT] & 0x80) || (input->keystate[DIK_D] & 0x80)))
+	{
+		if(objMgr->getObject()->getPhysics())
+		{
+			objMgr->getObject()->getPhysics()->walkingOff();
+		}
+	}
+
+	if ((input->keystate[DIK_B] & 0x80))
+	{
+		if(now - input->keyLag[DIK_B] > 200)
+		{
+			objMgr->getObject()->togglePhysics();
+			input->keyLag[DIK_B] = now;
+		}
+	}
+
+	if ((input->keystate[DIK_G] & 0x80))
+	{
+		if(now - input->keyLag[DIK_G] > 200)
+		{
+			objMgr->indexPrev();
+			input->keyLag[DIK_G] = now;
+		}
+	}
+	if ((input->keystate[DIK_H] & 0x80))
+	{
+		if(now - input->keyLag[DIK_H] > 200)
+		{
+			objMgr->indexNext();
+			input->keyLag[DIK_H] = now;
+		}
+	}
+
+	if ((input->keystate[DIK_P] & 0x80))
+	{
+		if(now - input->keyLag[DIK_P] > 200)
+		{
+			objMgr->pop();
+			input->keyLag[DIK_P] = now;
+		}
+	}
+
+
+			//if ((input->keystate[DIK_P] & 0x80))
+		//{
+		//	if(now - input->keyLag[DIK_P] > 200)
+		//	{
+		//		lvl1->getManager()->pop();
+		//		input->keyLag[DIK_P] = now;
+		//	}
+		//}
+		////test key
+		//if((input->keystate[DIK_E] & 0x80))
+		//{
+		//	if(now - input->keyLag[DIK_E] >200)
+		//	{
+		//		lvl1->~level();
+		//		lvl1->initLevel(dxMgr, "testfiles2.txt");
+		//	}
+		//}
+
+
+
+
+			//// camera movement
+		///*if ((keystate[DIK_NUMPAD4] & 0x80) || (keystate[DIK_J] & 0x80))
+		//{
+		//	if(now - keyLag[DIK_NUMPAD4] > cameraLag)
+		//	{
+		//		cameraX-= cameraMove;
+		//		keyLag[DIK_NUMPAD4] = now;
+		//	}
+		//}
+		//if ((keystate[DIK_NUMPAD6] & 0x80) || (keystate[DIK_L] & 0x80))
+		//{
+		//	if(now - keyLag[DIK_NUMPAD6] > cameraLag)
+		//	{
+		//		cameraX += cameraMove;
+		//		keyLag[DIK_NUMPAD6] = now;
+		//	}
+		//}
+		//if ((keystate[DIK_NUMPAD2] & 0x80) || (keystate[DIK_K] & 0x80))
+		//{
+		//	if(now - keyLag[DIK_NUMPAD2] > cameraLag)
+		//	{
+		//		cameraY -= cameraMove;
+		//		keyLag[DIK_NUMPAD2] = now;
+		//	}
+		//}
+		//if ((keystate[DIK_NUMPAD8] & 0x80) || (keystate[DIK_I] & 0x80))
+		//{
+		//	if(now - keyLag[DIK_NUMPAD8] > cameraLag)
+		//	{
+		//		cameraY+= cameraMove;
+		//		keyLag[DIK_NUMPAD8] = now;
+		//	}
+		//}*/
+		//
+		//if ((input->keystate[DIK_NUMPAD7] & 0x80) || (input->keystate[DIK_U] & 0x80))
+		//{
+		//	if(now - input->keyLag[DIK_NUMPAD7] > cameraLag)
+		//	{
+		//		if(cameraZ < -1.1)
+		//		{
+		//			cameraZ += cameraMove;
+		//			input->keyLag[DIK_NUMPAD7] = now;
+		//		}
+		//	}
+		//}
+		//if ((input->keystate[DIK_NUMPAD9] & 0x80) || (input->keystate[DIK_O] & 0x80))
+		//{
+		//	if(now - input->keyLag[DIK_NUMPAD9] > cameraLag)
+		//	{
+		//		cameraZ -= cameraMove;
+		//		input->keyLag[DIK_NUMPAD9] = now;
+		//	}
+		//}
+}
+
 void level::moveObject(D3DXVECTOR3 go)
 {
-	ObjMan->moveObject(go);
+	objMgr->moveObject(go);
 }
 level::~level()
 {
 	m_map->~cubeMap();
-	ObjMan->~objectManager();
+	objMgr->~objectManager();
 	files->~stringArray();
 }
