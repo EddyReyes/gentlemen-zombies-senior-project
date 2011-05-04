4be7cce1359fc270ba9bcb6764ac150fcca7bf55
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-04 22:45:55 +0000

    * cleaned up HUD, eddy, its ready for you.

--- trunk/ZombieGentlemen_SeniorProject/HUD.cpp
+++ trunk/ZombieGentlemen_SeniorProject/HUD.cpp
@@ -11,7 +11,6 @@ HUD::HUD()
 	weapon = NULL;
 	bagOfMoney = NULL;
 	PlayerID = NULL;
-	PlayerIDNumber = NULL;
 	PlayerMoney = NULL;
 	plyr = NULL;
 }
@@ -69,7 +68,6 @@ void HUD::draw()
 	if(PlayerMoney)
 	PlayerMoney->draw();
 }
-
 /****************************************************************
 * loadFromFile
 * takes in one text file, and loads images into an image manager
@@ -83,11 +81,14 @@ void HUD::loadFromFile(std::string filename, dxManager * a_dxMgr)
 	imgMgr->initImageManager(filename, dxMgr);
 
 	hudBackground = new HudImage(dxMgr, imgMgr->getImage(0), imgMgr->getImageInfo(0));
-
-
-
-
-
+	PlayerID = new HudImage(dxMgr, imgMgr->getImage(1), imgMgr->getImageInfo(1));
+	barHolder = new HudImage(dxMgr, imgMgr->getImage(2), imgMgr->getImageInfo(2));
+	healthBar = new HudImage(dxMgr, imgMgr->getImage(3), imgMgr->getImageInfo(3));
+	barHolder2 = new HudImage(dxMgr, imgMgr->getImage(4), imgMgr->getImageInfo(4));
+	armorBar = new HudImage(dxMgr, imgMgr->getImage(5), imgMgr->getImageInfo(5));
+	weapon = new HudImage(dxMgr, imgMgr->getImage(6), imgMgr->getImageInfo(6));
+	bagOfMoney = new HudImage(dxMgr, imgMgr->getImage(7), imgMgr->getImageInfo(7));
+	PlayerMoney = new DXText(dxMgr, imgMgr->getImage(8), imgMgr->getImageInfo(8));
 	// after loading all the images, load text paramerts for all DXText objects
 	PlayerMoney->loadFromTxtFile("textParameters.txt");
 }
@@ -133,169 +134,3 @@ void HUD::initDefaultPositions(float a_x, float a_y)
 * must be called in order for the updates to take effect
 ****************************************************************/
 void HUD::setPlayer(Player * a_Player){plyr = a_Player;}
-
-//The following methods must be removed*********************************
-
-void HUD::updateHealthBarDamage()
-{
-	float decrement;
-	plyr->PlayerDamage(15);
-	decrement = plyr->getHealth();
-	healthBar = new HudImage(dxMgr, "images/healthBar.bmp");
-	healthBar->setParameters(decrement, 12.0, 95.0, 43.0);
-}
-void HUD::useHealthPack()
-{
-	float increment;
-	plyr->healthRegenerate(20);
-	increment = plyr->getHealth();
-	healthBar = new HudImage(dxMgr, "images/healthBar.bmp");
-	healthBar->setParameters(increment, 12.0, 95.0, 43.0);
-}
-
-
-// the following methods have been commented to avoid errors when compiling
-// the reason they generate errors is becuase there is no longer an armor pointer
-
-
-
-//void HUD::updateArmorBarDamage(int a_armorType)
-//{
-//	float decrement;
-//	float stdWidth = 10.0;
-//	decrement = armor->getArmorHealth() - stdWidth;
-//	//armor->initPlayerArmor(a_armorType);
-//	//armor->damageArmor(6);
-//	decrement = armor->getArmorHealth();
-//	armorBar = new HudImage(dxMgr, "images/armorBar.bmp");
-//	armorBar->setParameters(decrement, 12.0, 95.0, 71.0);
-//}
-//void HUD::useArmorPickUp()
-//{
-//	float newArmor;
-//	armor->armorHealthRestore();
-//	newArmor = armor->getArmorHealth();
-//	armorBar = new HudImage(dxMgr, "images/armorBar.bmp");
-//	armorBar->setParameters(newArmor, armorBar->getYPosition(), 95.0, 71.0);
-//}
-void HUD::updateWeapon(std::string filename)
-{
-	if(weapon)
-	{	
-		weapon = new HudImage(dxMgr, filename);
-		weapon->setParameters(80.0, 80.0, 98.0, 97.0);
-	}
-}
-void HUD::updateCurrencyValue()
-{
-	char updateBuffer[50];
-  	sprintf_s(updateBuffer, "Mny %.00f/50\n Hp %.00f", plyr->getMoney(), healthBar->getWidth());
-	PlayerMoney->setDialog(updateBuffer);
-}
-
-void HUD::setHudImage(std::string filename)
-{
-	if(hudBackground == NULL)
-	{
-		hudBackground = new HudImage(dxMgr, filename);
-	}
-	else if(hudBackground)
-	{
-		hudBackground->~HudImage();
-		hudBackground = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setPlayerIDImage(std::string filename)
-{
-	if(PlayerID == NULL)
-	{
-		PlayerID = new HudImage(dxMgr, filename);
-	}
-	else if(PlayerID)
-	{
-		PlayerID->~HudImage();
-		PlayerID = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setBarHolderImage(std::string filename)
-{
-	if(barHolder == NULL)
-	{
-		barHolder = new HudImage(dxMgr, filename);
-	}
-	else if(barHolder)
-	{
-		barHolder->~HudImage();
-		barHolder = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setHealthBarImage(std::string filename)
-{
-	if(healthBar == NULL)
-	{
-		healthBar = new HudImage(dxMgr, filename);
-	}
-	else if(healthBar)
-	{
-		healthBar->~HudImage();
-		healthBar = new HudImage(dxMgr, filename);
-		
-	}
-}
-void HUD::setBarHolder2Image(std::string filename)
-{
-	if(barHolder2 == NULL)
-	{
-		barHolder2 = new HudImage(dxMgr, filename);
-	}
-	else if(barHolder2)
-	{
-		barHolder2->~HudImage();
-		barHolder2 = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setArmorBarImage(std::string filename)
-{
-	if(armorBar == NULL)
-	{
-		armorBar = new HudImage(dxMgr, filename);
-	}
-	else if(armorBar)
-	{
-		armorBar->~HudImage();
-		armorBar = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setWeaponImage(std::string filename)
-{
-	if(weapon == NULL)
-	{
-		weapon = new HudImage(dxMgr, filename);
-	}
-	else if(weapon)
-	{
-		weapon->~HudImage();
-		weapon = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setBagOfMoneyImage(std::string filename)
-{
-	if(bagOfMoney == NULL)
-	{
-		bagOfMoney = new HudImage(dxMgr, filename);
-	}
-	else if(bagOfMoney)
-	{
-		bagOfMoney->~HudImage();
-		bagOfMoney = new HudImage(dxMgr, filename);
-	}
-}
-void HUD::setCurrencyValue(std::string filename)
-{
-	//PlayerMoney = new DXText(dxMgr, filename);
-	//PlayerMoney->textInfo("Arial", 11,
-	//			D3DCOLOR_ARGB(255, 0, 0, 0), "Loading");
-	//PlayerMoney->setTextBoxParameters(68, 55, 72, 100, 12);
-
-
-}
--- trunk/ZombieGentlemen_SeniorProject/HUD.h
+++ trunk/ZombieGentlemen_SeniorProject/HUD.h
@@ -17,11 +17,9 @@ class HUD
 		HudImage * bagOfMoney;
 		HudImage * PlayerID;
 		Player * plyr;
-		DXText * PlayerIDNumber;
 		DXText * PlayerMoney;
 		imageManager * imgMgr;
 		dxManager * dxMgr;
-
 	public:
 		HUD();
 		~HUD();
@@ -30,22 +28,4 @@ class HUD
 		void update();
 		void initDefaultPositions(float a_x, float a_y);
 		void setPlayer(Player * a_Player);
-
-		// the following functions must be removed
-		void updateHealthBarDamage();
-		void useHealthPack();
-		void updateArmorBarDamage(int a_armorType);
-		void useArmorPickUp();
-		void updateWeapon(std::string filename);
-		void updateCurrencyValue();
-		void setHudImage(std::string filename);
-		void setPlayerIDImage(std::string filename);
-		void setBarHolderImage(std::string filename);
-		void setHealthBarImage(std::string filename);
-		void setBarHolder2Image(std::string filename);
-		void setArmorBarImage(std::string filename);
-		void setWeaponImage(std::string filename);
-		void setBagOfMoneyImage(std::string filename);
-		void setCurrencyValue(std::string filename);
-		
 };
