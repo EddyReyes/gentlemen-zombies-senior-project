16953eb1c49197242e16e5fca09c98c9593dd73c
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-07 21:57:37 +0000

    filled out some entity functionality

--- ZombieGentlemen_SeniorProject/entity.cpp
+++ ZombieGentlemen_SeniorProject/entity.cpp
@@ -1,1 +1,27 @@
 #include "entity.h"
+
+// constructor destructor
+entity::entity()
+{
+	m_object = NULL;
+}
+entity::~entity()
+{
+	// to destroy objects, tell the object manager to pop this object
+}
+
+// member functions
+void entity::animate()
+{
+	// this function needs more design before it is implimented
+}
+
+// mutators
+void entity::setObject(object * a_object){m_object = a_object;}
+void entity::entityDead(){alive = false;}
+void entity::entityAlive(){alive = true;}
+
+//accessors
+object * entity::getObject(){return m_object;}
+bool entity::isAlive(){return alive;}
+bool entity::hasArmor(){return armor;}
