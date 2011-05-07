35628e0b40342175cd1b8085d447db4dccf22a17
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-07 22:18:52 +0000

    * updated ziggy class a little bit n town constructor n destructor

--- ZombieGentlemen_SeniorProject/ziggy.cpp
+++ ZombieGentlemen_SeniorProject/ziggy.cpp
@@ -1,1 +1,16 @@
 #include "ziggy.h"
+
+void ziggy::load()
+{
+	
+}
+void ziggy::update()
+{
+	////update per frame
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	pos = m_object->getPosition();
+	//
+	//}
+
+}
--- ZombieGentlemen_SeniorProject/ziggy.h
+++ ZombieGentlemen_SeniorProject/ziggy.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "entity.h"
+#include "dxManager.h"
 
 class ziggy: public entity
 {
-
+private:
+	D3DXVECTOR3	* pos;
+public:
+	void load();
+	void update();
 };
