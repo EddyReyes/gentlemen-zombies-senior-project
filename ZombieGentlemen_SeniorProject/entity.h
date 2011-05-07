4fcc0bf810e4d4029ce226e1bc170799fedeb5c8
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-07 21:05:56 +0000

    filled out many of the entity requirements

--- ZombieGentlemen_SeniorProject/entity.h
+++ ZombieGentlemen_SeniorProject/entity.h
@@ -1,6 +1,32 @@
 #pragma once
+#include "object.h"
 
 class entity
 {
+protected:
+	object * m_object;
+	bool alive, armor;
+	// entity will also require a state
 
+
+public:
+	// constructor destructor
+	entity();
+	~entity();
+
+	// member functions
+	void animate();
+	
+	//virtual functions (for child classes)
+	virtual void update();
+
+	// mutators
+	void setObject(object * a_object);
+	void entityDead();
+	void entityAlive();
+
+	//accessors
+	object * getObject();
+	bool isAlive();
+	bool hasArmor();
 };
