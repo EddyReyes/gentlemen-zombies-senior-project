d22453ad9fca350fba721325c152975ddd324a5f
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-10 16:18:23 +0000

    * update ziggy class, needs a lot of work and revision, took out the struct in CustomVertex.h, and added a store image

--- ZombieGentlemen_SeniorProject/ziggy.cpp
+++ ZombieGentlemen_SeniorProject/ziggy.cpp
@@ -4,97 +4,28 @@
 /***********************************************************************
  * Makes the ziggy enemy(bird/fish) fly or swim in a wave like pattern * 
  ***********************************************************************/
-void ziggy::yMovement()
-{
-	//ziggy enemy up/down movement
-	if (ziggyMovement.posY > 450 &&
-		ziggyMovement.posY < 550)
-	{
-		ziggyMovement.moveY *= 1; // THIS STATEMENT COULD BE REMOVED, TECHNICALLY IT DOESNT REALLY DO ANYTHING
-	}
-	else ziggyMovement.moveY *= -1;
-
-	/************************************************************************
-	* COMMENT
-	* THIS LOGIC WILL DO THE FOLLOWING
-	IF THE STATEMENT if (ziggyMovement.posY > 450 &&
-						 ziggyMovement.posY < 550)
-
-	RETURNS TRUE, THEN IT WILL MULTIPLY BY ONE, ESSENTIALLY MAKING IT EAQUAL ITSELF,
-
-	IF FALSE THEN IT WILL BECOME NEGATIVE WHICH IS THE DESIRED EFFECT, SO REMOVING THE FIRST
-	STATEMENT SHOULD BE SAFE
-
-	************************************************************************/
-
-
-}
 void ziggy::load()
 {
 	//will load the ziggy flying enemy in game maps
+	m_object->setSprite(0, 1);
+	m_object->getCollData();
+	m_object->checkCollision();
 
 }
-void ziggy::update()
+void ziggy::update(float timePassed)
 {
 	/***************************************************************** 
 	 * Will update ziggy's position per frame and as soon            *
-	 * as the enemy flys or swims from one side to the other it will *
-	 * delete from memory                                            *
+	 * as the enemy flys or swims from one side to the other         *
 	 *****************************************************************/
-
-	// FOR NOW DONT WORRY TOO MUCH ABOUT DELETING IT FROM MEMORY, THIS WILL BE
-	// COVERED LATER
-
-
-	//assuming that the ziggy enemy starts at the left side of the screen
-	for (int i = 0; i < 10; i++)
-	{
-	ziggyMovement.posX += ziggyMovement.moveX;
-	//ziggy moving up and down in a waving motion
-	yMovement();
-	
-		//if the position is greater than the 800 or to the right side of the screen,
-		//then move back in the opposite direction
-		if(ziggyMovement.posX > WINDOW_WIDTH)
-		{
-			ziggyMovement.moveX *= -1;
-			yMovement();
-		}
-		//if the position is greater than the 0, then move back in the opposite direction
-		if(ziggyMovement.posX < 0)
-		{
-			ziggyMovement.moveX *= 1;
-			yMovement();
-		}
-		//if the position is greater than or equal to 800, then delete ziggy enemy
-		if(ziggyMovement.posX >= WINDOW_WIDTH)
-		{
-			//need to delete a ziggy object at this point,
-			//need help with using a object pointer for ziggy
-		}
-	}
-
-	/************************************************************************
-	* COMMENT
-	* A NEW DATA TYPE WAS CREATED TO FACILITATE THIS. ITS NOT PART OF THE DESIGN,
-	IM NOT GOING TO REMOVE IT, BUT YOU SHOULD NOT BE USING IT. THE FOLLOWING ARE 
-	INSTRUCTIONS FOR ACCESSING THE POSITION DATA OF THE OBJECT
-
-	1. make a D3DXVECTOR3 * and call it pos. It is safe to do it within this function,
-	since it is temporary. It does not need to be a part of the actual class.
-	2. retreive the position pointer of the object like this:  m_object->getPosition();
-	and assign it to pos.
-	3. now pos points to the position data for the object. From here you can manipulate the
-	position data like so: pos->x += moveX;
-	4. Calculate your movement data, which can also be created inside this function.
-	5. Manipulate the position data of the object using the move data.
-	6. remember to update the collision rect like this: m_object->getCollisionRect()->update();
-	7. Your done, the object should be moveing now
-
-	IF YOU HAVE MORE QUESTIONS LET ME KNOW. I WOULD COMMENT ON YOUR CURRENT LOGIC, BUT SEEING AS
-	ITS NOT USING THE RIGHT DATA, I WOULD RATHER WE FIX THAT FIRST, THEN GET BACK TO THE LOGIC,
-	ALSO ANY LOGIC USING THE WINDOW WIDTH ISNT GOING TO WORK, THE WINDOW WIDTH DOESNT AFFECT THE GAME
-	WORLD. AVOID USING WINDOW WIDTH.
-	************************************************************************/
-
+	D3DXVECTOR3 * pos;
+	pos = m_object->getPosition();
+	if ( pos->x < 800)
+	pos->x += 0.5f * timePassed;
+	//here I need physics to drop ziggy, then make him accelerate up
+	else if ( pos->x > 800 )
+	pos->x *= -0.5f * timePassed;
+	//here I need physics to drop ziggy, then make him accelerate up
+	//?????????
+	m_object->getCollisionRect()->update();
 }
--- ZombieGentlemen_SeniorProject/ziggy.h
+++ ZombieGentlemen_SeniorProject/ziggy.h
@@ -5,12 +5,8 @@
 class ziggy: public entity
 {
 private:
-	D3DXVECTOR3	* pos;
-
-	// THIS IS NEW DATA, ITS NOT PART OF THE DESIGN
-	spriteMovement ziggyMovement;
+	
 public:
-	void yMovement();
 	void load();
-	void update();
+	void update(float timePassed);
 };
