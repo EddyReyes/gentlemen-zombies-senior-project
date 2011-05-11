1fa46bd920ced3a49bcde526e3d8de71259eaab8
shoutingchief@gmail.com <shoutingchief@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-11 00:11:47 +0000

    Setup states for the turret. turret has collisionRect.obj.

--- ZombieGentlemen_SeniorProject/turret.cpp
+++ ZombieGentlemen_SeniorProject/turret.cpp
@@ -1,1 +1,37 @@
 #include "turret.h"
+
+turret::turret()
+{
+	time = 0;
+}
+turret::~turret()
+{
+
+}
+
+void turret::update(float timePassed)
+{
+	time += timePassed;
+	if(time <= 1.0f)
+	{
+		switch(state)
+		{
+		case fortyFiveRight:
+			m_object->setSprite(0, 0);
+			state = nindy;
+			break;
+
+		case nindy:
+			m_object->setSprite(0, 1);
+			state = fortyFiveLeft;
+			break;
+
+		case fortyFiveLeft:
+			m_object->setSprite(0, 2);
+			state = fortyFiveRight;
+			break;
+		}
+	}
+
+	m_object->getCollisionRect()->update();
+}
--- ZombieGentlemen_SeniorProject/turret.h
+++ ZombieGentlemen_SeniorProject/turret.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "entity.h"
 
+enum turretState {fortyFiveRight, nindy, fortyFiveLeft};
+
 class turret: public entity
 {
+private:
+	turretState state;
+	float time;
 
+public:
+
+	turret();
+
+	~turret();
+	void update(float timePassed);
 };
