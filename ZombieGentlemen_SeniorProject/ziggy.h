8ca886be4073e1dfd44f6957af0523777d59d330
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-14 05:25:12 +0000

    * cleaned up and put the armor mantle pic in the hud and more ziggy revisions

--- ZombieGentlemen_SeniorProject/ziggy.h
+++ ZombieGentlemen_SeniorProject/ziggy.h
@@ -2,14 +2,12 @@
 #include "entity.h"
 
 enum ziggyStates {fly1, fly2, fly3, deadZiggy};
-const float PI = 3.14159265;
 
 class ziggy: public entity
 {
 private:
 	ziggyStates state;
 public:
-	void load();
 	void update(float timePassed);
 	void animate();
 };
