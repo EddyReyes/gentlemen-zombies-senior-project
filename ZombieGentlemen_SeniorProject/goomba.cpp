d6dbcaf536bcd863d3aa8352fec1febdf455a35c
neurotoxin408@gmail.com <neurotoxin408@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-17 23:56:54 +0000

    goomba works now

--- ZombieGentlemen_SeniorProject/goomba.cpp
+++ ZombieGentlemen_SeniorProject/goomba.cpp
@@ -4,7 +4,7 @@ goomba::goomba()
 {
 	type = enemyEntity;
 	m_object = NULL;
-	state = right;
+	state = goombaWalkLeft;
 }
 
 goomba::~goomba()
@@ -17,16 +17,42 @@ goomba::~goomba()
 **/
 void goomba::update(float timePassed)
 {
-	D3DXVECTOR3 temp = *m_object->getPosition();
-	switch(state)
+	if(m_object->getPhysics())
 	{
-	case left:temp.x-=1*timePassed;break;
-	case right:temp.x+=1*timePassed;break;
-	case am_dead:break;
-	default:
-		assert(state>=max_states);
+
+		//check if goomba has reached a corner and turn around
+		int collData = m_object->getCollData();
+		//if goomba reaches right side of ledge, turn around
+		if(collidingLeft && !(collidingRight) && collidingBottom)
+		{
+			state = goombaWalkLeft;
+		}
+		// if goomba reaches left side of ledge turn around
+		if(collidingRight && !(collidingLeft) && collidingBottom)
+		{
+			state = goombaWalkRight;
+		}
+		// if goomba hits a wall
+		if(collidingRight && collidingLeft && collidingTop && collidingBottom)
+		{
+			// if goomba was previously walking left switch to right, and vise versa
+			if(state == goombaWalkRight)
+				state = goombaWalkLeft;
+			else if(state == goombaWalkLeft)
+				state = goombaWalkRight;
+		}
+
+		// apply movement
+		switch(state)
+		{
+		case goombaWalkLeft: m_object->getPhysics()->setXVelocity(-3.0f);
+			break;
+		case goombaWalkRight: m_object->getPhysics()->setXVelocity(3.0f);
+			break;
+		case goombaDead: m_object->getPhysics()->setXVelocity(0);
+			break;
+		}
 	}
-	m_object->setPosition(temp);
 }
 void goomba::animate()
 {
