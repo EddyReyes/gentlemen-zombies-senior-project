1e0b59096ec54457787bbd727a466e5be4ec138f
shoutingchief@gmail.com <shoutingchief@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-17 23:51:19 +0000

    Have an ideal for projectile so am just running with it. turret is down with.

--- ZombieGentlemen_SeniorProject/projectile.cpp
+++ ZombieGentlemen_SeniorProject/projectile.cpp
@@ -3,6 +3,7 @@
 projectile::projectile()
 {
 	m_object = NULL;
+	state = fire;
 }
 projectile::~projectile()
 {
@@ -11,5 +12,23 @@ projectile::~projectile()
 
 void projectile::update()
 {
+	D3DXVECTOR3 * pos;
+	pos = m_object->getPosition();
+
+	switch(state)
+		{
+
+			case fire:
+
+				m_object->setSprite(0, 0);
+				//pos->x += 0.5f * timePassed;
+				m_object->getCollisionRect()->update();
+				break;
+
+			case hit:
+
+				m_object->setSprite(0, 1);
+				break;
+		}
 
 }
--- ZombieGentlemen_SeniorProject/turret.cpp
+++ ZombieGentlemen_SeniorProject/turret.cpp
@@ -15,23 +15,7 @@ void turret::update(float timePassed)
 	time += timePassed;
 	if(time <= 1.0f)
 	{
-		switch(state)
-		{
-		case fortyFiveRight:
-			m_object->setSprite(0, 0);
-			state = ninety;
-			break;
-
-		case ninety:
-			m_object->setSprite(0, 1);
-			state = fortyFiveLeft;
-			break;
-
-		case fortyFiveLeft:
-			m_object->setSprite(0, 2);
-			state = fortyFiveRight;
-			break;
-		}
+		animate();
 	}
 
 	// the turrets do not move, so they dont really need up go through this extra step
@@ -40,5 +24,88 @@ void turret::update(float timePassed)
 
 void turret::animate()
 {
+	switch(wallState)
+	{
+	case faceRight:
 
+		switch(state)
+			{
+			case fortyFiveRight:
+				m_object->setSprite(0, 0);
+				state = ninety;
+				break;
+
+			case ninety:
+				m_object->setSprite(0, 1);
+				state = fortyFiveLeft;
+				break;
+
+			case fortyFiveLeft:
+				m_object->setSprite(0, 2);
+				state = fortyFiveRight;
+				break;
+			}
+		break;
+	case faceDown:
+
+		switch(state)
+			{
+			case fortyFiveRight:
+				m_object->setSprite(0, 0);
+				state = ninety;
+				break;
+
+			case ninety:
+				m_object->setSprite(0, 1);
+				state = fortyFiveLeft;
+				break;
+
+			case fortyFiveLeft:
+				m_object->setSprite(0, 2);
+				state = fortyFiveRight;
+				break;
+			}
+		break;
+
+	case faceUP:
+		switch(state)
+			{
+			case fortyFiveRight:
+				m_object->setSprite(0, 0);
+				state = ninety;
+				break;
+
+			case ninety:
+				m_object->setSprite(0, 1);
+				state = fortyFiveLeft;
+				break;
+
+			case fortyFiveLeft:
+				m_object->setSprite(0, 2);
+				state = fortyFiveRight;
+				break;
+			}
+		break;
+
+	case faceLeft:
+		switch(state)
+			{
+			case fortyFiveRight:
+				m_object->setSprite(0, 0);
+				state = ninety;
+				break;
+
+			case ninety:
+				m_object->setSprite(0, 1);
+				state = fortyFiveLeft;
+				break;
+
+			case fortyFiveLeft:
+				m_object->setSprite(0, 2);
+				state = fortyFiveRight;
+				break;
+			}
+		break;
+		
+	}
 }
