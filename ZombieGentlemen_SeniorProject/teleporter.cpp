cc457c6205cbd23dce87eae5bfba2fc79fbf744e
mknoche@gmail.com <mknoche@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-18 00:17:34 +0000

    committing stuff i plan working on, also a revised TODO list.txt

--- ZombieGentlemen_SeniorProject/teleporter.cpp
+++ ZombieGentlemen_SeniorProject/teleporter.cpp
@@ -1,1 +1,35 @@
 #include "teleporter.h"
+
+teleporter::teleporter()
+{
+	m_object = NULL;
+	to_port = NULL;
+	animate_state=0;
+}
+teleporter::~teleporter()
+{
+}
+void teleporter::update(float timePassed)
+{
+	time += timePassed;
+	if(time >= 0.04f)
+	{
+		animate();
+		time = 0;
+	}
+}
+void teleporter::animate()
+{
+	switch(animate_state)
+	{
+	case 0:
+		m_object->setSprite(0, 0);
+		animate_state = 1;
+	case 1:
+		m_object->setSprite(0, 1);
+		animate_state = 2;
+	case 2:
+		m_object->setSprite(0, 2);
+		animate_state = 0;
+	}
+}
--- ZombieGentlemen_SeniorProject/teleporter.h
+++ ZombieGentlemen_SeniorProject/teleporter.h
@@ -1,6 +1,16 @@
 #pragma once
+#include "entity.h"
 
-class teleporter
+
+class teleporter:public entity
 {
-
+public:
+	teleporter();
+	~teleporter();
+	void update(float timePassed);
+	void animate();
+private:
+	object* to_port;//object being teleported
+	float time;
+	int animate_state;
 };
