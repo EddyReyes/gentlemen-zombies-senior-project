52a1630e52192a4218191bff233af1f72a07a244
shoutingchief@gmail.com <shoutingchief@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-05-24 23:58:10 +0000

    armor is now being show on screen. its just a green box.

--- trunk/ZombieGentlemen_SeniorProject/armor.cpp
+++ trunk/ZombieGentlemen_SeniorProject/armor.cpp
@@ -1,15 +1,27 @@
 #include "armor.h"
 
-armor::armor(void)
+armor::armor()
 {
+	time = 0;
+	m_object = NULL;
 }
 
 armor::~armor(void)
 {
+
 }
 
 void armor::update(float timePassed)
-{}
+{
+	time += timePassed;
+
+	animate();
+
+	
+	
+}
 
 void armor::animate()
-{}
+{
+	m_object->setSprite(0,0);
+}
