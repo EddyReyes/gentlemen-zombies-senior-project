c210674cfa9c67990c36b7c53ead33b460323230
will.badal@gmail.com <will.badal@gmail.com@66a8e42f-0ee8-26ea-976e-e3172d02aab5>
2011-06-01 05:29:45 +0000

    * changed player.cpp animation()s.

--- trunk/ZombieGentlemen_SeniorProject/player.h
+++ trunk/ZombieGentlemen_SeniorProject/player.h
@@ -3,11 +3,13 @@
 #include "entity.h"
 
 enum playerState{idle, walkingRight, walkingLeft, jumping, dying, dead};
+enum playerSprite{playerSprite1, playerSprite2, playerSprite3, playerSprite4, playerSprite5};
 
 class player: public entity
 {
 private:
 	playerState state;
+	playerSprite sprites;
 	float timer, armorTimeout;
 	bool armorBlink;
 
